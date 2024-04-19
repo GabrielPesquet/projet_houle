80c67d37e19ce68d200f2891686b3451be25785c
Gabriel <gabriel@pesquet.eu>
2024-04-19 12:03:50 +0200

    Ajout de ignore, update rapide sur le reste

--- grapher.py
+++ grapher.py
@@ -9,8 +9,8 @@ setrecursionlimit(10000)
 hdata= np.fromfile("data.bin", dtype=float, count=-1, sep="") 
 hdata = hdata.reshape(1000,200, 200)
 
-forme_grille = {'width_ratios': (0.9, 0.05), 'wspace': 0.2}
-fig, (ax, cbar_ax) = plt.subplots(1, 2, gridspec_kw= forme_grille, figsize= (12, 8))
+forme_grille = {'width_ratios': (0.9, 0.04), 'wspace': 0.2}
+fig, (ax, cbar_ax) = plt.subplots(1, 2, gridspec_kw= forme_grille, figsize= (12, 12))
 
 m = hdata.min()
 M = hdata.max()
@@ -24,7 +24,8 @@ def updateheatmap(i):
                 xticklabels=20,
                 yticklabels=20,
                 vmin=m,
-                vmax=M)
+                vmax=M,
+                cmap="Blues",)
 
 anim = FuncAnimation(fig = fig, func = updateheatmap, frames = 500, interval = 10, blit = False)
 #anim.save("houle.mp4", fps= 10, dpi=200)
--- houle.c
+++ houle.c
@@ -4,7 +4,7 @@
 
 #define XMAX 200 // 200 utilisés + 150 de sécurité + 50 d'amortissement pour être sûr et certain qu'il n'y a pas d'effets de bord 
 #define YMAX 200 
-#define TMAX 5.0 // nombre de secondes de la simulation dans le monde réel 
+#define TMAX 30.0 // nombre de secondes de la simulation dans le monde réel 
 #define NTIMES 1000.
 #define MODEPROF 1 // 1 si basse profondeur, 0 si haute profondeur 
 #define NONDES 1
@@ -12,7 +12,7 @@
 double prof[2*XMAX][YMAX] ;
 double hauteur[2*XMAX][YMAX] ; 
 const double dt = TMAX/NTIMES; //pas de temps arbitraire
-const double dl = 0.1; //correspond à la distance entre deux cases
+const double dl = 1; //correspond à la distance entre deux cases
 const double g = 9.81;
 const double pi = 3.141592 ;
 
@@ -62,18 +62,20 @@ void init(){
 	for (int x = 0; x < 2*XMAX; x++){
 		for (int y=0; y < YMAX; y++){
 			hauteur[x][y] = 0;
-			prof[x][y] = sq((1 - (double) x/(2*XMAX)))*2 + sq((double) y/YMAX - 0.5)*4 + 4;
+			prof[x][y] = 10; 
 		}
 	}	
 
-	ondes[0] = new_onde(5.);
+	ondes[0] = new_onde(50.);
 }
 
 double coeffrot(int x){
 	if (x < 2*XMAX - 50){
 		return 0.; 
 	}	
-	return 30*exp(-1/(dl*(2*XMAX-x)));
+	
+	//if (x <= 50) return 30*exp(-1/sq(dl*x));
+	return 30*exp(-1/sq(dl*(2*XMAX-x)));
 }
 
 void futur_onde(onde w, int x, int y) {
@@ -90,7 +92,9 @@ void bords_onde(onde w, double t){
 	double c;
 	for (int y = 0; y < YMAX; y++){
 		c = calc_c(w.lambda, 0, y);
-		w.champ[2][0][y] = sin(t*2.*pi*c/w.lambda); // pour l'instant amplitude de 1m //wtf pas du tout 1 m finalement 
+		//w.champ[2][0][y] = exp(-sq(t/dt - 10)); 
+		w.champ[2][0][y] = sin(2*pi*t*c/w.lambda);  
+
 	}
 	
 }
@@ -104,18 +108,25 @@ void update_onde(onde w, double t){
 	w.champ[2] = temp ;
 
 	//calcul du nouveau futur 
-	bords_onde(w,t);
 	for (int x = 1; x < 2*XMAX - 1 ; x++){
 		for (int y = 0; y < YMAX; y++){
 			futur_onde(w,x,y);
 		}
 	}
+
+	bords_onde(w,t);
 }
 
 void update_h(double t){
+	for (int x = 0; x < XMAX ; x++){
+		for (int y = 0; y < YMAX ; y++){
+			hauteur[x][y] = 0 ;
+		}
+	}
+
 	for (int i = 0; i < NONDES ; i++){
 		update_onde(ondes[i], t); 
-		for (int x = 0; x < 2*XMAX ; x++){
+		for (int x = 0; x < XMAX ; x++){
 			for (int y = 0 ; y < YMAX ; y ++ ){
 				hauteur[x][y] += ondes[i].champ[1][x][y] ;
 			}
