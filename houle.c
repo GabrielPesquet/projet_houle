786e69b4bac118bf7a43e477f52e0fa2ff724e97
Gabriel <gabriel@pesquet.eu>
2024-04-19 17:16:20 +0200

    Ajout des conditions de bord de Dirichlet

--- houle.c
+++ houle.c
@@ -2,15 +2,16 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-#define XMAX 200 // 200 utilisés + 150 de sécurité + 50 d'amortissement pour être sûr et certain qu'il n'y a pas d'effets de bord 
+#define XMAXS 200 
+#define XMAXR 205
 #define YMAX 200 
-#define TMAX 30.0 // nombre de secondes de la simulation dans le monde réel 
-#define NTIMES 1000.
+#define TMAX 120.0 // nombre de secondes de la simulation dans le monde réel 
+#define NTIMES 4000.
 #define MODEPROF 1 // 1 si basse profondeur, 0 si haute profondeur 
 #define NONDES 1
 
-double prof[2*XMAX][YMAX] ;
-double hauteur[2*XMAX][YMAX] ; 
+double prof[XMAXR][YMAX] ;
+double hauteur[XMAXR][YMAX] ; 
 const double dt = TMAX/NTIMES; //pas de temps arbitraire
 const double dl = 1; //correspond à la distance entre deux cases
 const double g = 9.81;
@@ -27,8 +28,8 @@ onde new_onde(double longueur_onde){
 	double *** champ = malloc(3 * sizeof(double**) );
 	
 	for (int i = 0; i < 3; i++){	
-		champ[i] = malloc(2*XMAX*sizeof(double*));
-		for (int x = 0; x < 2*XMAX; x++){
+		champ[i] = malloc(XMAXR*sizeof(double*));
+		for (int x = 0; x < XMAXR; x++){
 			champ[i][x] = malloc(YMAX * sizeof(double));
 			for (int y = 0; y < YMAX; y++){
 				champ[i][x][y] = 0 ; 
@@ -46,8 +47,10 @@ double sq(double x) {
 }
 
 double laplacien(double ** champ, int x , int y) {
+
 	if (y==0) return (champ[x+1][y] + champ[x-1][y] + champ[x][y+1] + champ[x][YMAX-1] - 4.*champ[x][y])/(dl*dl);
 	if (y==YMAX-1) return (champ[x+1][y] + champ[x-1][y] + champ[x][0] + champ[x][y-1] - 4.*champ[x][y])/(dl*dl);
+
 	return (champ[x+1][y] + champ[x-1][y] + champ[x][y+1] + champ[x][y-1] - 4.*champ[x][y])/(dl*dl);
 }
 
@@ -58,11 +61,10 @@ double calc_c(double lambda, int x, int y){
 }
 
 void init(){
-	//sera remplacé par un fichier à part avec différents préset plus tard si j'ai la foi 
-	for (int x = 0; x < 2*XMAX; x++){
+	for (int x = 0; x < XMAXR; x++){
 		for (int y=0; y < YMAX; y++){
 			hauteur[x][y] = 0;
-			prof[x][y] = 10 + (1-0.5 * (double) x/XMAX)*4 + sq((double) y/YMAX - 0.5)*4; 
+			prof[x][y] = 10 + (1-0.5 * (double) x/XMAXR)*4 + sq((double) y/YMAX - 0.5)*4; 
 		}
 	}	
 
@@ -70,12 +72,15 @@ void init(){
 }
 
 double coeffrot(int x){
-	if (x < 2*XMAX - 50){
+	/*
+	if (x < 3*XMAX/2){
 		return 0.; 
 	}	
 	
-	//if (x <= 50) return 30*exp(-1/sq(dl*x));
-	return 30*exp(-1/sq(dl*(2*XMAX-x)));
+	return exp(-1/sq(0.04*(2*XMAX-x)));
+	*/
+
+	return 0;
 }
 
 void futur_onde(onde w, int x, int y) {
@@ -88,7 +93,9 @@ void futur_onde(onde w, int x, int y) {
 }
 
 void bords_onde(onde w, double t){
-	// pour l'instant un onde plane harmonique venant de x=0
+	//doit être appelée après le calcl du futur du reste
+
+	//bord haut pour l'instant un onde plane harmonique venant de x=0
 	double c;
 	for (int y = 0; y < YMAX; y++){
 		c = calc_c(w.lambda, 0, y);
@@ -96,6 +103,16 @@ void bords_onde(onde w, double t){
 		w.champ[2][0][y] = sin(2*pi*t*c/w.lambda);  
 
 	}
+
+	//bord bas 
+	double kappa ; //juste pour les calculs 
+	for (int y = 0; y < YMAX; y++){
+		c = calc_c(w.lambda, 0, y);
+		kappa = c*dt/dl ; 
+		w.champ[2][XMAXR - 1][y] = w.champ[1][XMAXR- 2][y]
+								  + (w.champ[2][XMAXR - 2][y] - w.champ[1][XMAXR-1][y])*(kappa - 1)/(kappa + 1) ;
+	}
+
 	
 }
 
@@ -108,7 +125,7 @@ void update_onde(onde w, double t){
 	w.champ[2] = temp ;
 
 	//calcul du nouveau futur 
-	for (int x = 1; x < 2*XMAX - 1 ; x++){
+	for (int x = 1; x < XMAXR - 1 ; x++){
 		for (int y = 0; y < YMAX; y++){
 			futur_onde(w,x,y);
 		}
@@ -118,7 +135,7 @@ void update_onde(onde w, double t){
 }
 
 void update_h(double t){
-	for (int x = 0; x < XMAX ; x++){
+	for (int x = 0; x < XMAXS ; x++){
 		for (int y = 0; y < YMAX ; y++){
 			hauteur[x][y] = 0 ;
 		}
@@ -126,16 +143,18 @@ void update_h(double t){
 
 	for (int i = 0; i < NONDES ; i++){
 		update_onde(ondes[i], t); 
-		for (int x = 0; x < XMAX ; x++){
+		for (int x = 0; x < XMAXR ; x++){
 			for (int y = 0 ; y < YMAX ; y ++ ){
 				hauteur[x][y] += ondes[i].champ[1][x][y] ;
 			}
 		}
 	}
+
+	//printf("%lf", hauteur[XMAX+10][YMAX/2]);
 }
 
 void savebin(FILE * fp){
-	for (int x = 0; x < XMAX; x++){
+	for (int x = 0; x < XMAXS; x++){
 		for (int y = 0 ; y < YMAX; y++){
 			fwrite(&hauteur[x][y], sizeof(double), 1, fp);
 		}
