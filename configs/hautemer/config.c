33f8753a5fda8df68ccca7471992159878e00c1e
GabrielPesquet <110241484+GabrielPesquet@users.noreply.github.com>
2024-10-31 20:27:53 +0100

    Add files via upload

---
+++ configs/hautemer/config.c
@@ -0,0 +1,74 @@
+#include "config.h" 
+
+/* le but de ce set est purement d'étudier des interférences
+ * ondulatoires, sans jouer sur la profondeur */
+
+double max(double a, double b){
+	return a < b ? b : a ; 
+}
+
+void init_cste(double cste)
+{
+	for (int x = 0; x < XMAX; x++)
+	{
+		for (int y = 0; y < YMAX; y++)
+		{
+			hauteur[x][y] = 0;
+			prof[x][y] = cste; // ! A 4 ca dvg, pas à 3.........
+							   // prof[x][y] = 10 + (1-0.5 * (double) x/XMAX)*4 + sq((double) y/YMAX - 0.5)*6;
+		}
+	}
+
+	ondes[0] = new_onde(50.);
+	ondes[1] = new_onde(100.);
+}
+
+
+void init()
+{
+	init_cste(4.); // Partout hors du plan
+}
+
+
+void limites(double t){
+	for (int i = 0 ; i < NONDES ; i++){
+		bords(ondes[i]);
+		limites_mur_rect(ondes[i], 0,  YMAXS/2 - 2, XMAX-1, YMAXS/2 + 2);		
+	}
+
+	limites_onde_point(ondes[0], XMAXS/2, YMAXS/4, t); 
+	limites_onde_point(ondes[1], XMAXS/2, 3*YMAXS/4, t); 
+
+}
+
+void bords(onde w){
+	if (MODECIRC / 2 == 0){
+		for (int x = 0 ; x < XMAX ; x++){
+			w.champ[2][x][0] = 0.  ; w.champ[2][x][YMAX - 1] = 0. ;
+		}
+	}
+	if (MODECIRC % 2 == 0) { //bords durs verticaux
+		for (int y = 0 ; y < YMAX ; y++){
+			w.champ[2][0][y] = 0. ; w.champ[2][XMAX - 1][y] = 0. ; 
+		}
+	}
+}
+
+void limites_onde_point(onde w, int xc, int yc, double t)
+	/* comme une goutte qui tombe */
+{
+	double c;
+	//fprintf(stderr, "Au bord : %lf, ", gaussian(YMAX/6, mu, sigma));
+	//fprintf(stderr, "au centre : %lf\n", gaussian(YMAX/2, mu, sigma));
+	c = calc_c(w.lambda, xc, yc);
+	w.champ[2][xc][yc] = 5 *sin(2 * pi * t * c / w.lambda);
+
+}
+
+void limites_mur_rect(onde w, int x1, int y1, int x2, int y2){
+	for (int x = x1 ; x <= x2 ; x ++ ){
+		for (int y = y1 ; y <= y2 ; y++){
+			w.champ[2][x][y] = 0 ; 
+		}
+	}
+}
